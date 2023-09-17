a71390f678356137a09e9bafd8e899e9fa1dff82
Eli T. Drumm <eli@eli.td>
2023-09-17 15:58:33 -0400

    Add .zshrc.d/vars/path.sh Add .zshrc.d/vars/vars.sh

---
+++ dot_zshrc.d/vars/path.sh
@@ -0,0 +1,13 @@
+
+typeset -U path PATH
+path=(
+  $path
+  /home/winfield/scripts
+  /home/winfield/bin
+  /home/winfield/apps
+  /home/winfield/.local/bin
+  /home/winfield/.cargo/bin
+  .
+)
+
+export PATH
---
+++ dot_zshrc.d/vars/vars.sh
@@ -0,0 +1,30 @@
+export HOME=/home/winfield
+
+
+
+export EDITOR='micro'
+
+export XDG_DATA_HOME=/home/winfield/.local/share
+export XDG_CACHE_HOME=/home/winfield/.cache
+export XDG_CONFIG_HOME=/home/winfield/.config
+export XDG_STATE_HOME=/home/winfield/.local/state
+
+export XDG_SESSION_TYPE=wayland
+#export GBM_BACKEND=nvidia-drm
+#export __GLX_VENDOR_LIBRARY_NAME=nvidia
+#export GLX_VENDOR_LIBRARY_NAME=nvidia
+export QT_QPA_PLATFORM=wayland
+
+#export DBUS_SESSION_BUS_ADDRESS=unix:path=/run/usr/1000/bus
+
+export SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/ssh-agent.socket"
+
+export GOPATH="~/.go"
+
+export USE_GKE_GCLOUD_AUTH_PLUGIN=True
+
+export AMD_VULKAN_ICD=RADV
+
+
+export MANPAGER="sh -c 'col -bx | bat -l man -p'"
+export MANROFFOPT="-c"
