5ae7ccc2a43a9dbf0582110ed53bf6dca07bda5f
Eli T. Drumm <eli@eli.td>
2023-10-23 23:28:23 -0400

    Update .config/chezmoi/chezmoistate.boltdb Update .zshrc Update .zshrc.d/completions/main.zsh

--- dot_zshrc.d/completions/main.zsh
+++ dot_zshrc.d/completions/main.zsh
@@ -1,2 +1,7 @@
 
 source /usr/bin/aws_zsh_completer.sh
+
+# tabtab source for packages
+# uninstall by removing these lines
+[[ -f ~/.config/tabtab/zsh/__tabtab.zsh ]] && . ~/.config/tabtab/zsh/__tabtab.zsh || true
+
